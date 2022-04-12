3217c6ca0a9463197cc5d1662bd3af1649c6d261
Daniel Roth <d.roth@esri.ch>
2022-04-12 10:38:56 +0200

    Exception parameter to not register as versioned

--- arcpy/register_data_as_versioned.py
+++ arcpy/register_data_as_versioned.py
@@ -2,9 +2,17 @@ import os
 import sys
 import traceback
 import arcpy
+import ast
 
 sde_file_path = sys.argv[1]
 sde_file_name = sys.argv[2]
+exceptions_list = None
+
+try:
+    if len(sys.argv) is 4:
+        exceptions_list = str(sys.argv[3])
+except:
+    print("no exception datasets")
 
 ws = os.path.join(sde_file_path, sde_file_name)
 
@@ -17,29 +25,53 @@ arcpy.env.workspace = ws
 print('start registering data as versioned in {} ...'.format(ws))
 
 try:
+    # handle None
+    if exceptions_list is None:
+        exceptions_list = []
+    else:
+        # parse string to list, e.g. "['GENIUSDB_DKM25.DKM25_STATUSGITTER','GENIUSDB_DKM25.GN_BLOCKEINTEILUNG','GENIUSDB_DKM25.LK25_BLATTSCHNITT','GENIUSDB_DKM25.WU_PERIMETER']"
+        # To handle this string representation of a list as a script parameter do not use blanks inside the string.
+        # Otherwise sys.argv is going to handle the blanks like parameter separation.
+        exceptions_list = [n.strip() for n in ast.literal_eval(exceptions_list)]
+        print('exceptional datasets {}'.format(exceptions_list))
+
+except:
+    print(traceback.format_exc())
+    input()
+
+try:
     datasets = arcpy.ListDatasets()
     for ds in datasets:
-        path = os.path.join(arcpy.env.workspace, ds)
-        print(' register as versioned: {}'.format(ds))
-        try:
-            arcpy.management.RegisterAsVersioned(os.path.join(ws, ds), 'NO_EDITS_TO_BASE')
-        except:
-            print(traceback.format_exc())
-            # e.g. if there are relationships between feature classes!
-            print ('trying one more time...')
-            arcpy.management.RegisterAsVersioned(os.path.join(ws, ds), 'NO_EDITS_TO_BASE')
+        if ds in exceptions_list:
+            print(' don\'t register as versioned: {}'.format(ds))
+            continue
+        else:
+            print(' register as versioned: {}'.format(ds))
+            try:
+                arcpy.management.RegisterAsVersioned(os.path.join(ws, ds), 'NO_EDITS_TO_BASE')
+            except:
+                print(traceback.format_exc())
+                # e.g. if there are relationships between feature classes!
+                print('trying one more time...')
+                arcpy.management.RegisterAsVersioned(os.path.join(ws, ds), 'NO_EDITS_TO_BASE')
 
     fcs = arcpy.ListFeatureClasses()
     for fc in fcs:
-        path = os.path.join(arcpy.env.workspace, fc)
-        print(' register as versioned: {}'.format(fc))
-        arcpy.management.RegisterAsVersioned(os.path.join(ws, fc), 'NO_EDITS_TO_BASE')
+        if fc in exceptions_list:
+            print(' don\'t register as versioned: {}'.format(fc))
+            continue
+        else:
+            print(' register as versioned: {}'.format(fc))
+            arcpy.management.RegisterAsVersioned(os.path.join(ws, fc), 'NO_EDITS_TO_BASE')
 
     tables = arcpy.ListTables()
     for table in tables:
-        path = os.path.join(arcpy.env.workspace, table)
-        print(' register as versioned: {}'.format(table))
-        arcpy.management.RegisterAsVersioned(os.path.join(ws, table), 'NO_EDITS_TO_BASE')
+        if table in exceptions_list:
+            print(' don\'t register as versioned: {}'.format(table))
+            continue
+        else:
+            print(' register as versioned: {}'.format(table))
+            arcpy.management.RegisterAsVersioned(os.path.join(ws, table), 'NO_EDITS_TO_BASE')
 
     print('')
     print('finished registering data as versioned')
@@ -47,4 +79,3 @@ try:
 except:
     print(traceback.format_exc())
     input()
-
